72bf3f30b707399fa89a0a61a97177d822d776f9
ItaChi <128603283+i8ab@users.noreply.github.com>
2026-08-13 01:47:11 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import DictionaryApp from "./App.jsx";
+import ErrorBoundary from "./components/layout/ErrorBoundary.jsx";
 import "./index.css";
 
 const root = document.getElementById("root");
 createRoot(root).render(
   <React.StrictMode>
-    <DictionaryApp />
+    <ErrorBoundary>
+      <DictionaryApp />
+    </ErrorBoundary>
   </React.StrictMode>
 );
